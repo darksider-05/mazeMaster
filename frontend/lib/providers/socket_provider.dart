2e0cdbb7d88371049bc4ceb32095b9ce08894cda
darksider-05 <a.bahrkazemi84@gmail.com>
2026-07-06 00:59:41 +0330

    websocket initialization

--- frontend/lib/providers/socket_provider.dart
+++ frontend/lib/providers/socket_provider.dart
@@ -4,9 +4,10 @@ import 'package:mazemaster/providers/game_provider.dart';
 import "package:web_socket_channel/web_socket_channel.dart";
 
 class SocketProvider {
-  late final WebSocketChannel channel;
+  final WebSocketChannel channel;
 
-  SocketProvider(GameProvider game) {
+  SocketProvider(GameProvider game)
+    : channel = WebSocketChannel.connect(Uri.parse("ws://localhost:8765")) {
     channel.stream.listen((message) {
       game.applyMessage(jsonDecode(message));
     });
